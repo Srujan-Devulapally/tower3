22493421d9a7384a2278c9068b175fce8f3b4da8
Srujan-Devulapally <76732774+Srujan-Devulapally@users.noreply.github.com>
2021-04-12 11:26:06 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,32 @@
+class Box{
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.2
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+    this.visibility = 255
+  }
+  display(){
+    if(this.body.speed<3){
+      var angle = this.body.angle;
+      var pos = this.body.position;
+      push()
+      translate(pos.x, pos.y)
+      rotate(angle)
+      rect(0, 0, this.width, this.height)
+      pop()
+    }
+    else{
+      World.remove(world, this.body)
+      push()
+      this.visibility = this.visibility-5
+      tint(255, this.visibility)
+      pop()
+    }
+  }
+}
